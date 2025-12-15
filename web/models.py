e511d3a9cdd05a1a92345d5e8d66f21d6f86a319
}1H1KXJ)2O9t <s314577@studenti.polito.it>
2025-12-16 00:27:38 +0100

    added the expense of 16 december 2025

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Expense (models.Model):
+    description = models.TextField()
+    data = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user =models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
