76958970578bfaa32d307774b30756446df2b9a6
}1H1KXJ)2O9t <s314577@studenti.polito.it>
2025-12-18 21:30:58 +0100

    fixing minor issues

--- web/models.py
+++ web/models.py
@@ -9,3 +9,13 @@ class Expense (models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user =models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return f"{self.amount}-{self.date}"
+
+class Income(models.Model):
+    description = models.TextField()
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete = models.SET_NULL, null=True)
+    def __str__(self):
+        return f"{self.amount}-{self.date}"
